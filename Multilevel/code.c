dae5ee81c99089d6f43ea29948962046dea11e4d
Aravind226 <aravindbn2006@gmail.com>
2026-06-12 08:20:40 +0530

    remaining tasks

--- Multilevel/code.c
+++ Multilevel/code.c
@@ -4,11 +4,11 @@
 
 int main() {
     int n, i;
-    int PID[MAX], AT[MAX], BT[MAX];
+    int PID[MAX], AT[MAX], BT[MAX], TYPE[MAX];
     int CT[MAX], TAT[MAX], WT[MAX], rem_BT[MAX];
     int completed = 0;
     int time = 0;
-    int tq = 4;
+    int tq = 3;
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
@@ -20,13 +20,15 @@ int main() {
         scanf("%d", &AT[i]);
         printf("Burst Time: ");
         scanf("%d", &BT[i]);
+        printf("Type (0 = System, 1 = User): ");
+        scanf("%d", &TYPE[i]);
         rem_BT[i] = BT[i];
     }
 
     while(completed < n) {
         int done = 0;
         for(i = 0; i < n; i++) {
-            if(AT[i] <= time && rem_BT[i] > 0) {
+            if(TYPE[i] == 0 && AT[i] <= time && rem_BT[i] > 0) {
                 int run = rem_BT[i];
                 if(run > tq) {
                     run = tq;
@@ -41,7 +43,23 @@ int main() {
             }
         }
         if(done) {
-            printf("P%d ", PID[i]);
+            continue;
+        }
+        int idx = -1;
+        for(i = 0; i < n; i++) {
+            if(TYPE[i] == 1 && AT[i] <= time && rem_BT[i] > 0) {
+                if(idx == -1 || AT[i] < AT[idx]) {
+                    idx = i;
+                }
+            }
+        }
+        if(idx != -1) {
+            time += 1;
+            rem_BT[idx] -= 1;
+            if(rem_BT[idx] == 0) {
+                CT[idx] = time;
+                completed++;
+            }
             continue;
         }
         int next = 999999;
@@ -55,12 +73,12 @@ int main() {
         }
     }
 
-    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
+    printf("\nPID\tAT\tBT\tTYPE\tCT\tTAT\tWT\n");
     for(i = 0; i < n; i++) {
         TAT[i] = CT[i] - AT[i];
         WT[i] = TAT[i] - BT[i];
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n",
-        PID[i], AT[i], BT[i],
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        PID[i], AT[i], BT[i], TYPE[i],
         CT[i], TAT[i], WT[i]);
     }
 
